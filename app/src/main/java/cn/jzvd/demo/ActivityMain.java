4ee3aabc536692a9e1cc4f4b28c300cd82d55ada
Tide <yangguangrush@gmail.com>
2020-11-27 17:28:11 +0800

    1

--- app/src/main/java/cn/jzvd/demo/ActivityMain.java
+++ app/src/main/java/cn/jzvd/demo/ActivityMain.java
@@ -3,11 +3,13 @@ package cn.jzvd.demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import cn.jzvd.Jzvd;
 import cn.jzvd.demo.CustomJzvd.MyJzvdStd;
@@ -28,9 +30,21 @@ public class ActivityMain extends AppCompatActivity {
 
         myJzvdStd.setUp("http://jzvd.nathen.cn/342a5f7ef6124a4a8faf00e738b8bee4/cf6d9db0bd4d41f59d09ea0a81e918fd-5287d2089db37e62345123a1be272f8b.mp4"
                 , "饺子快长大");
-        Glide.with(this).load("http://jzvd-pic.nathen.cn/jzvd-pic/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png").into(myJzvdStd.thumbImageView);
+        Glideurl(myJzvdStd.thumbImageView,"http://jzvd-pic.nathen.cn/jzvd-pic/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png");
     }
+    public  void Glideurl(ImageView imageView, Object url) {
+            try {
+                Glide.with(imageView.getContext())
+                        .setDefaultRequestOptions(
+                                new RequestOptions()
+                                        .centerCrop()
+                        )
+                        .load(url)
+                        .into(imageView);
+            } catch (Exception e) {
 
+            }
+    }
     @Override
     protected void onPause() {
         super.onPause();
